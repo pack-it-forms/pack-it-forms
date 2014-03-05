081930cf91ebe9b5cedba5ee7b7ee0bc82b357ac
Peter Amidon <peter@picnicpark.org>
2014-03-05 07:05:02 -0800

    Fix handling of empty query strings

--- resources/js/pack-it-forms.js
+++ resources/js/pack-it-forms.js
@@ -491,8 +491,9 @@ function combobox_other_manager(e) {
 /* Utility: generate an object from the query string */
 function query_string_to_object() {
     var query = {};
-    string = window.location.search.substring(1).split("&");
-    string.forEach(function(element, index, array) {
+    string = window.location.search.substring(1);
+    list = string ? string.split("&") : [];
+    list.forEach(function(element, index, array) {
         list = element.split("=");
         query[list[0]] = decodeURIComponent(list[1].replace("+", "%20"));
     });
