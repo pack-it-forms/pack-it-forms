5c1727c6054b03547078b7e4d0be6fcae80bf110
Keith Amidon <keith@awakenetworks.com>
2015-05-09 08:28:51 -0700

    Uprev pac-read version

--- resources/scripts/pac-read/setup.py
+++ resources/scripts/pac-read/setup.py
@@ -3,7 +3,7 @@ from cx_Freeze import setup, Executable
 
 build_exe_options = {"packages": ["sys", "os", "subprocess", "shutil", "winreg"]}
 setup (name = "pac-read",
-       version = "0.0.2",
+       version = "0.0.5",
        description = "pack-it-forms form display wrapper for PacFORMS pac-read.exe",
        authors = "pack-it-forms development team",
        options = {"build_exe": build_exe_options},
