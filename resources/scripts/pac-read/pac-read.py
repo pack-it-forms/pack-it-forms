c2c71d37e3d198c813b75d2353f641926f6335f5
Keith Amidon <camalot@picnicpark.org>
2016-04-03 10:36:06 -0700

    Update copyright years

--- resources/scripts/pac-read/pac-read.py
+++ resources/scripts/pac-read/pac-read.py
@@ -1,5 +1,5 @@
-# Copyright 2014 Keith Amidon
-# Copyright 2014 Peter Amidon
+# Copyright 2014, 2015, 2016 Keith Amidon
+# Copyright 2014, 2015, 2016 Peter Amidon
 #
 # Licensed under the Apache License, Version 2.0 (the "License");
 # you may not use this file except in compliance with the License.
