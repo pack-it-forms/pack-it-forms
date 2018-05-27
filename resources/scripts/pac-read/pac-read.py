fcef2e29cf1b091ba2e7abacc692b56afe208960
Keith Amidon <camalot@picnicpark.org>
2018-05-27 01:20:17 -0700

    Improve pac-read shim robustness and troubleshooting
    Recently the pac-read shim has started failing on some installations
    of LAARES members. Logging was not sufficient to narrow down the root
    cause, necessitating a trip to examine one of the machines that was
    failing. From that trip it was determined that the browser could not
    be launched successfully but it wasn't clear why.
    
    This commit makes several improvements to fix or at least allow more
    effective troubleshooting of the issue.
    
    Firstly, it replaces the way in which the browser command line to
    launch the default browser is determined. Previously it would retrieve
    the default browser command from the registry but just tried to
    extract the path to the browser command without using the full command
    template with the URL substituted as specified. With this commit I
    believe it now will.
    
    Secondly, troubleshooting was complicated by the inability to change
    the command to be run to launch the browser without editing the
    registry which could affect other things on the system and be
    difficult to recover from if done incorrectly. This commit adds the
    ability to read a browser command line template, in the same format it
    is specified in the registry, from a configuration file. It also adds
    the ability to enable debugging by setting a key in this file. An
    example file is included in the commit.
    
    Finally, since the information included in the log was not sufficient
    to troubleshoot, this commit adds significantly more debug logging. At
    startup it tries to verify the environment looks sane and suggest to
    the user what to do if it does not, logging all violations. During
    execution it is more explicit about the browser command line being
    used and generally captures much more error information in the log.
    
    Include the version number in the log
    
    Add a timestamp to log messages
    
    pac-read.py: format_browser_cmd() don't lose characters
    
    There were two places where characters were being incorrectly omitted
    from the result in the implementation. This fixes them.
    
    Fix quoting of required modules in pac-read setup.py

--- resources/scripts/pac-read/pac-read.py
+++ resources/scripts/pac-read/pac-read.py
@@ -21,7 +21,7 @@
 # pack-it-forms msg directory and opens the form.  If not, it calls
 # the original pac-read.exe with the same arguments it was called with.
 
-import sys, os, os.path, subprocess, shutil
+import sys, os, os.path, subprocess, shutil, configparser, datetime
 try:
     import winreg
 except ImportError as e:
@@ -29,7 +29,7 @@ except ImportError as e:
         "Dummy winreg class to allow testing on Linux"
         @staticmethod
         def QueryValue(tree, var):
-            return "\"firefox"
+            return '"firefox" "%1"'
         HKEY_CLASSES_ROOT=""
 
 # Assume the original PacFORMS pac-read.exe was renamed
@@ -38,29 +38,29 @@ except ImportError as e:
 script_directory = os.path.dirname(os.path.abspath(sys.argv[0]))
 original_pacread_exe = os.path.join(script_directory, "pac-read-pacforms.exe")
 
-# If an environment variable named PACKITFORMS_BASE exists, use
-# that as the location of pack-it-forms.  Otherwise assume that
-# this directory in which this script was run is the location
-# of pack-it-forms.
-try:
-    base_directory = os.environ["PACKITFORMS_BASE"]
-except KeyError:
-    base_directory = script_directory
-msg_directory = os.path.join(base_directory, "msgs");
+base_directory = os.environ.get("PACKITFORMS_BASE")
+if base_directory:
+    config_filename = os.path.join(base_directory, "CONFIG")
+    version_filename = os.path.join(base_directory, "VERSION")
+    msg_directory = os.path.join(base_directory, "msgs")
+else:
+    config_filename = None
+    version_file = None
+    msg_directory = None
 
 debug_logfile = None
+config = {}
 
 def main():
-    try:
-        if os.environ["PACKITFORMS_LOGLEVEL"] == "DEBUG":
-            debug_open()
-    except KeyError:
-        pass
-
-    debug("pac-read started as: {!r}", sys.argv)
+    read_config()
+    verify_environment()
+    version = get_version()
+    debug("pac-read {!s} started as: {!r}", version, sys.argv)
     if len(sys.argv) == 2 and sys.argv[1] == "--pack-it-forms-version":
-        print_version()
-    elif len(sys.argv) == 4:
+        print("pack-it-forms version: {!s}".format(version))
+        sys.exit(0)
+
+    if len(sys.argv) == 4:
         # The message data input filename is held in the third parameter
         msg_filename = sys.argv[3]
         msgno = os.path.basename(msg_filename).split("_")[0]
@@ -72,19 +72,41 @@ def main():
     else:
         pacforms_handler()
 
+def read_config():
+    global config
+    global config_filename
+    parser = configparser.ConfigParser(strict=False, interpolation=None,
+                                       default_section=None)
+    # Silently ignore a non-existent file
+    if config_filename:
+        parser.read(config_filename)
+
+    debug_default = os.environ.get("PACKITFORMS_LOGLEVEL") == "DEBUG"
+    config["debug"] = parser.getboolean("pac-read", "debug",
+                                        fallback=debug_default)
+    if config["debug"]:
+        debug_open()
+    if config_filename and os.path.exists(config_filename):
+        debug("Found config file at {!r}", config_filename)
+
+    config["browser_cmd_fmt"] = parser.get("pac-read", "browser",
+                                           fallback=registry_browser_cmd_fmt())
+
 def debug_open():
     global debug_logfile
     if debug_logfile:
         debug("debug_open() called on an already open file")
     else:
-        debug_logfile = open(os.path.join(base_directory, 'pac-read.log'), 'a')
-        debug("Opening debug log file")
+        if base_directory:
+            debug_logfile = open(os.path.join(base_directory, 'pac-read.log'), 'a')
+            debug("Opening debug log file")
 
 def debug(fmt, *arg):
     global debug_logfile
     if debug_logfile:
-        fmt = fmt + '\n'
-        debug_logfile.write(fmt.format(*arg))
+        ts = datetime.datetime.now().strftime("%Y-%m-%d %H:%M:%S.%f ")
+        fmt = '{!s}' + fmt + '\n'
+        debug_logfile.write(fmt.format(ts, *arg))
         debug_logfile.flush()
 
 def debug_close():
@@ -93,26 +115,100 @@ def debug_close():
         debug("Closing debug log file")
         debug_logfile.close()
 
-def print_version():
-    version_filename = os.path.join(base_directory, "VERSION")
+def registry_browser_cmd_fmt():
+    regkey = "\\http\\shell\\open\\command"
+    return winreg.QueryValue(winreg.HKEY_CLASSES_ROOT, regkey)
+
+def verify_environment():
+    global base_directory
+    global version_filename
+    global msg_directory
+    base_err = False
+    install_err = False
+    script_err = False
+    if not base_directory:
+        debug("PACKITFORMS_BASE environment variable is not set")
+        print("""
+The PACKITFORMS_BASE environment variable is not set.
+
+This variable must be set and point to the directory where pack-it-forms
+is installed.
+
+""")
+        sys.exit(1)
+    if not os.path.isdir(base_directory):
+        debug("Base directory does not exit: {!r}", base_directory)
+        if not base_err:
+              print("""\
+The directory specified in the PACKITFORMS_BASE environment variable
+does not exit:
+
+   {!s}
+
+""".format(base_directory))
+        base_err = True
+    if not os.path.isfile(version_filename):
+        debug("Version file does not exist: {!r}", version_filename)
+        if not base_err:
+              print("""
+The directory specified in PACKITFORMS_BASE does not have a VERSION
+file at:
+
+   {!s}
+
+Are you sure that PACKITFORMS_BASE is set to the correct directory?
+
+""".format(version_filename))
+        install_err = True
+
+    if not os.path.isdir(msg_directory):
+        debug("Message directory does not exist: {!r}", msg_directory)
+        if not (base_err or install_err):
+              print("""
+The directory specified in PACKITFORMS_BASE does not have a msgs
+subdirectory at:
+
+   {!s}
+
+Are you sure that PACKITFORMS_BASE is set to the correct directory?
+
+""".format(msg_directory))
+        install_err = True
+    if not os.path.isfile(original_pacread_exe):
+        debug("Original PacFORMS pac-read.exe not at: {!r}",
+              original_pacread_exe)
+        print("""
+The original PacFORMS pac-read.exe does not exist at:
+
+   {!s}
+
+Are you sure that you copied the original pac-read.exe file to
+that location and replaced it with this script?
+
+""".format(original_pacread_exe))
+        script_err = True
+    if base_err or install_err or script_err:
+        sys.exit(1)
+
+def get_version():
+    global version_filename
     try:
         f = open(version_filename, "r")
         v = f.readline().rstrip()
+        return v
     except Exception as e:
         debug("Exception: {!r}", e)
-        print("""It seems pack-it-forms is incorrectly installed.
-
-Verify that you set the "PACKITFORMS_BASE" environment variable
-correctly and that it contains a file named "VERSION".
-""")
-        sys.exit(1)
-    print(v)
+        return "unknown"
 
 def form_filename_from_msg(filename):
     global base_directory
     # Which form to open a message with is determined by the "#
     # FORMFILENAME: " line in the input file
-    file = open(filename)
+    try:
+        file = open(filename)
+    except Exception as e:
+        debug("Exception finding form filename in msg: {!r}", e)
+        raise e
     line = file.readline()
     while (not (line.startswith("# FORMFILENAME: ") or line == "")):
         line = file.readline();
@@ -122,29 +218,140 @@ def form_filename_from_msg(filename):
     return os.path.join(base_directory, line.split(":")[1].strip())
 
 def pack_it_forms_handler(form_filename, msg_filename, msgno):
-    global msg_directory
     debug("opening pack-it-form {!r} for msgno {!r} using data in {!r}",
                  form_filename, msgno, msg_filename)
+    copy_msg_to_msgno_in_pack_it_forms_msgs_dir(msg_filename, msgno)
+    url = "file:///" + form_filename + "?mode=readonly&msgno=" + msgno
     try:
-        # Copy the input file to the msg directory with filename msgno
-        dst_filename = os.path.join(msg_directory, msgno)
-        if os.path.abspath(msg_filename) != os.path.abspath(dst_filename):
-            shutil.copy(msg_filename, dst_filename)
-        else:
-            debug("Skipping copy because supplied file is already in msgs dir.")
+        retcode = open_pack_it_forms_msg_in_browser(url)
     except Exception as e:
+        debug("Exception: {!r}", e)
         print(e)
         sys.exit(1)
-    url = "file:///" + form_filename + "?mode=readonly&msgno=" + msgno
-    # Must open it with the default browser
-    regkey = "\http\shell\open\command"
-    regval = winreg.QueryValue(winreg.HKEY_CLASSES_ROOT, regkey)
-    sys.exit(subprocess.call([regval.split("\"")[1], url]))
+    if retcode != 0:
+        debug("browser returned error code {!r}", retcode)
+    sys.exit(retcode)
+
+def copy_msg_to_msgno_in_pack_it_forms_msgs_dir(msg_filename, msgno):
+    global msg_directory
+    try:
+        dst_filename = os.path.join(msg_directory, msgno)
+        if os.path.abspath(msg_filename) != os.path.abspath(dst_filename):
+            debug("Copying msg from {!r} to {!r}", msg_filename, dst_filename)
+            shutil.copy(msg_filename, dst_filename)
+        else:
+            debug("Skipping copy as supplied file is already in msgs dir.")
+    except Exception as e:
+        debug("Exception: {!r}", e)
+        print(e)
+        sys.exit(1)
+
+def open_pack_it_forms_msg_in_browser(url):
+    cmd = format_browser_cmd(url)
+    debug("opening pack-it-forms form in browser as: {!r}", cmd)
+    return subprocess.call(cmd)
+
+def format_browser_cmd(url):
+    # Ugly hand-written parser for windows command quoting as
+    # described in:
+    # https://docs.python.org/3/library/subprocess.html#converting-an-argument-sequence-to-a-string-on-windows
+    global config
+    cmd = []
+    arg = ""
+    pending_backslash = False
+    pending_percent = False
+    in_quotes = False
+    for c in config["browser_cmd_fmt"]:
+        if c == '\\':
+            if pending_percent:
+                arg += '%'
+                pending_percent = False
+            if pending_backslash:
+                arg += '\\\\'
+                pending_backslash = False
+            else:
+                pending_backslash = True
+        elif c == '"':
+            if pending_percent:
+                arg += '%'
+                pending_percent = False
+            if pending_backslash:
+                arg += '"'
+                pending_backslash = False
+            else:
+                in_quotes = not in_quotes
+        elif c == '%':
+            if pending_backslash:
+                arg += '\\%'
+                pending_backslash = False
+            if pending_percent:
+                arg += '%%'
+                pending_percent = False
+            else:
+                pending_percent = True
+        elif c == '1':
+            if pending_backslash:
+                arg += '\\'
+                pending_backslash = False
+            if pending_percent:
+                arg += url
+                pending_percent = False
+            else:
+                arg += '1'
+        elif c == ' ' or c == '\t':
+            if pending_backslash:
+                arg += '\\'
+                pending_backslash = False
+            if pending_percent:
+                arg += '%'
+                pending_percent = False
+            if in_quotes:
+                arg += c
+            else:
+                # This logic will drop quoted empty arguments. I don't
+                # think this will be a problem in this application,
+                # but we'll see. Supporting empty arguments requires
+                # more logic to distinguish that case from multiple
+                # whitespace characters between arguments.
+                if len(arg) > 0:
+                    cmd.append(arg)
+                    arg = ''
+        else:
+            if pending_backslash:
+                arg += '\\'
+                pending_backslash = False
+            if pending_percent:
+                arg += '%'
+                pending_percent = False
+            arg += c
+    if pending_backslash:
+        arg += '\\'
+        pending_backslash = False
+    if pending_percent:
+        arg += '%'
+        pending_percent = False
+    # This logic will drop quoted empty arguments. I don't
+    # think this will be a problem in this application,
+    # but we'll see. Supporting empty arguments requires
+    # more logic to distinguish that case from multiple
+    # whitespace characters between arguments.
+    if len(arg) > 0:
+        cmd.append(arg)
+        arg = ''
+    return cmd
 
 def pacforms_handler():
     global original_read_exe
     sys.argv[0] = original_pacread_exe
     debug("running PacFORMS pac-read as: {!r}", sys.argv)
-    sys.exit(subprocess.call(sys.argv))
+    try:
+        retcode = subprocess.call(sys.argv)
+    except Exception as e:
+        debug("Exception: {!r}", e)
+        print(e)
+        sys.exit(1)
+    if retcode != 0:
+        debug("PacFORMS pac-read.exe returned error code {!r}", retcode)
+    sys.exit(retcode)
 
 main();
--- resources/scripts/pac-read/setup.py
+++ resources/scripts/pac-read/setup.py
@@ -1,7 +1,8 @@
 import sys
 from cx_Freeze import setup, Executable
 
-build_exe_options = {"packages": ["sys", "os", "subprocess", "shutil", "winreg"]}
+build_exe_options = {"packages": ["sys", "os", "subprocess", "shutil",
+                                  "winreg", "configparser", "datetime"]}
 setup (name = "pac-read",
        version = "0.0.5",
        description = "pack-it-forms form display wrapper for PacFORMS pac-read.exe",
