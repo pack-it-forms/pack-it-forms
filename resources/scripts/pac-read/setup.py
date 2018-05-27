d0541f845fbf05548af05a66c2a5b53340d102ee
Keith Amidon <camalot@picnicpark.org>
2018-05-27 01:23:29 -0700

    Uprev to 0.0.8pre1
    This is a pre-release for testing on LAARES systems that are failing
    to start the browser with pack-it-forms forms.

--- resources/scripts/pac-read/setup.py
+++ resources/scripts/pac-read/setup.py
@@ -4,7 +4,7 @@ from cx_Freeze import setup, Executable
 build_exe_options = {"packages": ["sys", "os", "subprocess", "shutil",
                                   "winreg", "configparser", "datetime"]}
 setup (name = "pac-read",
-       version = "0.0.5",
+       version = "0.0.8",
        description = "pack-it-forms form display wrapper for PacFORMS pac-read.exe",
        authors = "pack-it-forms development team",
        options = {"build_exe": build_exe_options},
